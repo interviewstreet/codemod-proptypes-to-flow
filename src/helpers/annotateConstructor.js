efca9971e955e4414c477032c15288a4012b3dfa
Progyan Bhattacharya <Progyan@hackerrank.com>
2019-03-18 12:47:40 +0530

    Lint Error: Fix Linting issues and Ignore files
    Release major version: v1.0.0
    
    Signed-off-by: Progyan Bhattacharya <Progyan@hackerrank.com>

--- src/helpers/annotateConstructor.js
+++ src/helpers/annotateConstructor.js
@@ -2,12 +2,28 @@
  * Annotates ES2015 Class constructor and Class `props` member
  *
  * @param {jscodeshiftApi} j jscodeshift API
- * @param {Array} body Array of `Node`
+ * @param {Array} clazz Array of `Node`
  */
-export default function annotateConstructor(j, body, name = 'Props') {
+export default function annotateConstructor(j, clazz, name = 'Props') {
   const type = j.genericTypeAnnotation(j.identifier(name), null);
+  const typeAnnotation = j.typeAnnotation(
+    j.genericTypeAnnotation(j.identifier(name), null)
+  );
 
-  if (body.superClass && !body.superTypeParameters) {
-    body.superTypeParameters = j.typeParameterInstantiation([type]);
+  if (clazz.superClass && !clazz.superTypeParameters) {
+    clazz.superTypeParameters = j.typeParameterInstantiation([type]);
   }
+
+  const classBody = clazz.body && clazz.body.body;
+
+  classBody.some(node => {
+    if (node.kind === 'constructor') {
+      // first parameter is always props regardless of name
+      if (node.value.params && node.value.params.length) {
+        node.value.params[0].typeAnnotation = typeAnnotation;
+      }
+
+      return true;
+    }
+  });
 }
