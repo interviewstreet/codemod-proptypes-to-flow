4ee2918d3aaa5f47b22bfb052ec6b695f8f67602
Aakansha Doshi <aakansha1216@gmail.com>
2019-03-18 11:41:33 +0530

    Revert "Type Annotation: Prop in constructor param"
    This reverts commit 2fb5f38236dee20d30bf823a5d4ecefa9e10ba36.

--- src/helpers/annotateConstructor.js
+++ src/helpers/annotateConstructor.js
@@ -2,26 +2,12 @@
  * Annotates ES2015 Class constructor and Class `props` member
  *
  * @param {jscodeshiftApi} j jscodeshift API
- * @param {Array} clazz Array of `Node`
+ * @param {Array} body Array of `Node`
  */
-export default function annotateConstructor(j, clazz, name = 'Props') {
+export default function annotateConstructor(j, body, name = 'Props') {
   const type = j.genericTypeAnnotation(j.identifier(name), null);
-  const typeAnnotation = j.typeAnnotation(j.genericTypeAnnotation(j.identifier(name), null));
 
-  if (clazz.superClass && !clazz.superTypeParameters) {
-    clazz.superTypeParameters = j.typeParameterInstantiation([type]);
+  if (body.superClass && !body.superTypeParameters) {
+    body.superTypeParameters = j.typeParameterInstantiation([type]);
   }
-
-  const classBody = clazz.body && clazz.body.body;
-
-  classBody.some(node => {
-    if (node.kind === 'constructor') {
-      // first parameter is always props regardless of name	
-      if (node.value.params && node.value.params.length) {
-        node.value.params[0].typeAnnotation = typeAnnotation;
-      }
-
-      return true;
-    }
-  });
 }
